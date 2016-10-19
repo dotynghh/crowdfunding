65edba40ca480da1f3d82b27845168b5ff80e212
高 帅 <dotyng_hh@163.com>
2016-10-19 17:53:53 +0800

    view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+  	root :to => "users#index"
+  	resources :users
+  	resources :projects
+  end
 end
