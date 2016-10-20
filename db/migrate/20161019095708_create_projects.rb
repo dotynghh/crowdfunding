473d3f0ab9cc5291bd572bd7700185497894f700
高 帅 <dotyng_hh@163.com>
2016-10-20 17:24:51 +0800

    project new

--- db/migrate/20161019095708_create_projects.rb
+++ db/migrate/20161019095708_create_projects.rb
@@ -5,6 +5,9 @@ class CreateProjects < ActiveRecord::Migration[5.0]
     	t.integer :user_id
     	t.text :introduction
     	t.string :avatar
+      t.string :avatar_file_name
+      t.string :avatar_content_type
+      t.string :avatar_file_size
     	t.string :goals
     	t.string :category_id
     	t.datetime :start_at
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id           :integer          not null, primary key
+#  title        :string(255)
+#  user_id      :integer
+#  introduction :text(65535)
+#  avatar       :string(255)
+#  goals        :string(255)
+#  category_id  :string(255)
+#  start_at     :datetime
+#  end_at       :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
